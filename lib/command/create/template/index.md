7eb8226ad4c5c9ca45c8e3e5bcb85a33c7fdb2bf
sdhushu <1426732052@qq.com>
2023-03-25 18:34:32 +0800

    chore: command create dir

---
+++ lib/command/create/template/index.md
@@ -0,0 +1,11 @@
+# 组件名称
+
+### 介绍
+
+组件功能介绍
+
+<code src="./demo/index.tsx"></code>
+
+## API
+
+### 属性
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Options } from "tsup";
-
-export const tsup: Options = {
-  entry: ["src/*ts"],
-  format: ["cjs"],
-  dts: true,
-  splitting: true,
-  clean: true
-}
